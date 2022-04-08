f0bf6812194d081a0d771caf154ee9bf94f2aab7
EluciferE <luciferevgeniy@bk.ru>
2022-04-08 09:37:32 +0700

    updated ajax

--- MainParser/urls.py
+++ MainParser/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='main-index'),
     path('register', views.register, name='main-register'),
     path('logout', views.logout, name='main-logout'),
-    path('^ajax/get_table/$', views.get_table, name='get_table')
+    path('ajax/get_table/', views.get_table, name='get_table')
 ]
