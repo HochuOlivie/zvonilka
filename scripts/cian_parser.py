6f5be08e43e7fc4252cf7a01b4b13f3e05504c4f
EluciferE <luciferevgeniy@bk.ru>
2022-04-11 03:55:52 +0700

    logger for cian

--- scripts/cian_parser.py
+++ scripts/cian_parser.py
@@ -4,11 +4,19 @@ import re
 from bs4 import BeautifulSoup
 import inspect
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from .cian_cookies import cookies
 from MainParser.models import Ad
+import logging
 
+FORMAT = '[%(asctime)s] - [%(levelname)s] - %(message)s'
+logging.basicConfig(level=logging.INFO)
+cian = logging.FileHandler('logs/cian.log')
+cian.setFormatter(logging.Formatter(FORMAT))
 
+logger = logging.getLogger('cian')
+logger.addHandler(cian)
+logger.propagate = False
 
 headers = {
     'authority': 'www.cian.ru',
@@ -28,6 +36,7 @@ headers = {
     # 'cookie': '__cf_bm=vjX4fXMAkrQQOj72KL5RIFLYvXxQA6rF91xTw_xzkSs-1649608462-0-ARTxP+u4PZQe5LfAQXWC3DpoZDoPa4Czm8G9ldLvlCavamDuViBMjne+0OlXb4+BHwFre2LWXGVMreO1AXLgWHw=; _CIAN_GK=ad09d1d7-d870-4167-9849-5d2a56ceb81d; _gcl_au=1.1.799167971.1649608463; anti_bot="2|1:0|10:1649608483|8:anti_bot|40:eyJyZW1vdGVfaXAiOiAiMTk0Ljg3Ljk0LjQzIn0=|7658adc25d94861dffb6a8339001b9cc0ca5fed21f46ba49cf4124e3613bf64f"; session_region_id=1; adb=1; login_mro_popup=1; uxfb_usertype=searcher; sopr_utm=%7B%22utm_source%22%3A+%22direct%22%2C+%22utm_medium%22%3A+%22None%22%7D; sopr_session=3bacc1a0955048e5; _ga=GA1.2.1291009391.1649608495; _gid=GA1.2.1122705383.1649608495; _dc_gtm_UA-30374201-1=1; _ym_uid=1649608495222256227; _ym_d=1649608495; _ym_isad=2; uxs_uid=27923a50-b8ec-11ec-be3e-ada3716650b3; _ym_visorc=b',
 }
 
+
 class AvitoParser:
     title = ['.*row.*']
     link = ['.*link--eoxce']
@@ -38,13 +47,13 @@ class AvitoParser:
         self.user_attrs = {a[0]: a[1] for a in all_attributes if not (a[0].startswith('__') and a[0].endswith('__'))}
         self.user_attrs_compiled = {k: [re.compile(vv) for vv in v] for k, v in self.user_attrs.items()}
         self.cookie_counter = 0
-        
+
         self.session = requests.session()
         self.session.headers.update(headers)
         self.session.cookies.update(list(cookies.values())[self.cookie_counter])
         self.session.proxies.update({'https': list(cookies.keys())[self.cookie_counter]})
-        
-        #self.url = 'https://www.cian.ru/cat.php?deal_type=rent&engine_version=2&is_by_homeowner=1&offer_type=flat&region=1&room1=1&room2=1&room3=1&room4=1&room5=1&room6=1&sort=creation_date_desc&type=4'
+
+        # self.url = 'https://www.cian.ru/cat.php?deal_type=rent&engine_version=2&is_by_homeowner=1&offer_type=flat&region=1&room1=1&room2=1&room3=1&room4=1&room5=1&room6=1&sort=creation_date_desc&type=4'
         self.url = 'https://www.cian.ru/cat.php?deal_type=rent&engine_version=2&is_by_homeowner=1&offer_type=flat&region=1&sort=creation_date_desc&totime=3600&type=4'
 
     def get_ads(self):
@@ -59,13 +68,11 @@ class AvitoParser:
         except Exception as e:
             print(e)
             return []
-        
 
         soup = BeautifulSoup(page.text, 'html.parser')
         res = {}
         for k, v in self.user_attrs_compiled.items():
             res[k] = soup.find_all(lambda x: self._reg_equal(v, x.get('class')))
-
 
         ress = []
         for k, v in res.items():
@@ -94,7 +101,7 @@ class AvitoParser:
         return ress
 
     def get(self, url):
-        
+
         return self.session.get(url)
 
     # Check if all regexp are in second array
@@ -104,14 +111,32 @@ class AvitoParser:
         else:
             return False
 
+    def previous_proxy(self):
+        return list(cookies.keys())[self.cookie_counter]
+
+
 def run():
+    start_time = datetime.now()
+    proxy_stat = {x: {'ddos': 0, 'good': 0} for x in list(cookies.keys())}
+    total_ads = [0]
+
     ap = AvitoParser()
 
     while True:
 
+        if datetime.now() > start_time + timedelta(hours=2):
+            start_time = datetime.now()
+            logger.info('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+            logger.info(f"Total ads: {total_ads[0]}")
+            for proxy, statistic in proxy_stat.items():
+                logger.info(f"{proxy} - DDOS:{statistic['ddos']}, GOOD: {statistic['good']}")
+            proxy_stat = {x: {'ddos': 0, 'good': 0} for x in list(cookies.keys())}
+            total_ads = [0]
+
         last_ads = ap.get_ads()
         if len(last_ads) == 0:
             print("DDOS")
+            proxy_stat[ap.previous_proxy()]['ddos'] += 1
             time.sleep(30)
             continue
 
@@ -120,8 +145,11 @@ def run():
             if ad_db:
                 continue
 
+            total_ads[0] += 1
+            proxy_stat[ap.previous_proxy()]['good'] += 1
             print(ad)
-            Ad(date=datetime.now(), site='ci', title=ad['title'], address=ad['address'], price=ad['price'], phone=ad['phone'], city='Москва', person='', link=ad['link']).save()
+            Ad(date=datetime.now(), site='ci', title=ad['title'], address=ad['address'], price=ad['price'],
+               phone=ad['phone'], city='Москва', person='', link=ad['link']).save()
         time.sleep(2 * 60)
 
 # output = open('offers.xlsx', 'wb')
