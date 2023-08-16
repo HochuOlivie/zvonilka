93277c9cd2c022d9fb9a9d5b5da602cdc14d1389
Meow <luciferevgeniy@bk.ru>
2023-08-16 14:32:30 +0600

    tmp version

--- scripts/server_test.py
+++ scripts/server_test.py
@@ -7,8 +7,9 @@ import json
 from datetime import datetime, timedelta
 from threading import Thread
 
-from MainParser.models import Ad, User, Profile, TargetAd
+from MainParser.models import Ad, Profile, TargetAd
 
+import asyncpg
 from asgiref.sync import sync_to_async
 from scripts.server.client import Client
 import os
@@ -23,49 +24,97 @@ utc = pytz.UTC
 clients = []
 ready_calls = 0
 
+DB_AUTH = {
+    'database': os.getenv('DB_DATABASE', 'postgres'),
+    'user': os.getenv('DB_USER', 'postgres'),
+    'password': os.getenv('DB_PASSWORD', '17628312D'),
+    'host': os.getenv('DB_HOST', 'localhost'),
+    'port': os.getenv('DB_PORT', '5432'),
+}
 
-@sync_to_async
-def getAds():
-    return list(Ad.objects.filter(tmpDone=False, done=False, no_call=False).order_by('-id')[:50])
-
-
-async def getCalls():
-    global ready_calls
-    while True:
-        try:
-            ads = await getAds()
-            
-            calls = [call for call in ads if
-                     call.date + timedelta(minutes=2, hours=3) > utc.localize(datetime.now())]
-
-            calls = [call for call in calls if call.views <= 700 and call.phone != '+74954760059' and not call.clearColor]
-            
-            ready_calls = len(calls)
-            shuffle(clients)
-
-            for call in calls:
-                workers = [x for x in clients if
-                           x.ready and x.lastCall + timedelta(seconds=3) < datetime.now() and x.authorized]
-                for worker in workers:
-                    is_working = await worker.working()
-                    if not is_working:
-                        continue
-                    print(f"wanna call: {worker.ready}, {call.phone}")
-                    worker.ready = False
-                    await worker.makeCall(call)
-                    break
-
-        except Exception as e:
-            if DEBUG or 1==1:
-                print(f'Error: {e}')
-            await asyncio.sleep(5)
+DB_CREATE_CHANNEL = 'new_ads_channel'
+DB_UPDATE_CHANNEL = 'update_ads_channel'
 
 
-def calls_wrapper():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(getCalls())
-    loop.close()
+async def get_db_connection() -> asyncpg.Connection:
+    return await asyncpg.connect(**DB_AUTH)
+
+
+async def listen_create_channel(callback: callable):
+    conn = await get_db_connection()
+    await conn.add_listener(DB_CREATE_CHANNEL, callback)
+
+
+async def listen_update_channel(callback: callable):
+    conn = await get_db_connection()
+    await conn.add_listener(DB_UPDATE_CHANNEL, callback)
+
+
+# @sync_to_async
+# def getAds():
+#     return list(Ad.objects.filter(tmpDone=False, done=False, no_call=False).order_by('-id')[:50])
+
+
+async def on_create_ad(*args):
+    connection, pid, channel, payload = args
+    record: Ad = await sync_to_async(Ad.objects.get)(id=int(payload))
+    if record.noCall or record.phone == '+74954760059' or record.clearColor:
+        return
+    await make_call(record)
+
+
+async def make_call(ad: Ad):
+    shuffle(clients)
+
+    workers = [x for x in clients if
+               x.ready and x.lastCall + timedelta(seconds=3) < datetime.now() and x.authorized]
+
+    for worker in workers:
+        is_working = await worker.working()
+        if not is_working:
+            continue
+        worker.ready = False
+        await worker.makeCall(ad)
+
+
+# async def getCalls():
+#     global ready_calls
+#     while True:
+#         try:
+#             ads = await getAds()
+#
+#             calls = [call for call in ads if
+#                      call.date + timedelta(minutes=2, hours=3) > utc.localize(datetime.now())]
+#
+#             calls = [call for call in calls if
+#                      call.views <= 700 and call.phone != '+74954760059' and not call.clearColor]
+#
+#             ready_calls = len(calls)
+#             shuffle(clients)
+#
+#             for call in calls:
+#                 workers = [x for x in clients if
+#                            x.ready and x.lastCall + timedelta(seconds=3) < datetime.now() and x.authorized]
+#                 for worker in workers:
+#                     is_working = await worker.working()
+#                     if not is_working:
+#                         continue
+#                     print(f"wanna call: {worker.ready}, {call.phone}")
+#                     worker.ready = False
+#                     await worker.makeCall(call)
+#                     break
+#
+#         except Exception as e:
+#             if DEBUG or 1 == 1:
+#                 print(f'Error: {e}')
+#             await asyncio.sleep(5)
+
+
+# def calls_wrapper():
+#     loop = asyncio.new_event_loop()
+#     asyncio.set_event_loop(loop)
+#     loop.run_until_complete(getCalls())
+#     loop.close()
 
 
 async def checkTargets():
@@ -113,7 +162,10 @@ def gui():
         sleep(3)
 
 
-Thread(target=calls_wrapper).start()
+# Thread(target=calls_wrapper).start()
+
+# TODO: await listen_create_channel(on_create_ad)
+
 Thread(target=targets_wrapper).start()
 if not DEBUG:
     Thread(target=gui).start()
