af0eb0e203c1e9fc8166e35af0c4f76ff6c1fcf5
Meow <luciferevgeniy@bk.ru>
2024-01-15 14:17:13 +0000

    update glisa parser

--- scripts/glisa_parser.py
+++ scripts/glisa_parser.py
@@ -7,21 +7,38 @@ from django.utils import timezone
 import time
 
 cookies = {
-    'rapidshopref': 'http%3A%2F%2Fxn--80afpl5a.xn--p1ai%2F',
-    'rapidshoplogin': '06c757d9891dae2d2ff258f8c4965180',
+    'user_rnd': 'grf_1705324054_0',
+    '_ga': 'GA1.2.373876291.1705324056',
+    '_gid': 'GA1.2.1440359982.1705324056',
+    '_ym_uid': '1705324056275299195',
+    '_ym_d': '1705324056',
+    '_ym_isad': '1',
+    '_ym_visorc': 'w',
+    'rapidshopref': 'https%3A%2F%2Fxn--80afpl5a.xn--p1ai%2F',
+    'rapidshoplogin': '8297d8a343692c1dca1202873a1e901c',
     'rapidshopemail': 'Amaks996%40mail.ru',
     'user_code': 'grf_23071_1232410',
+    '_gat': '1',
+    '_ga_9TY92ZQNZT': 'GS1.2.1705324056.1.1.1705324145.0.0.0',
 }
 
+
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    # 'Accept-Encoding': 'gzip, deflate',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
-    # Requests sorts cookies= alphabetically
-    # 'Cookie': 'rapidshopref=http%3A%2F%2Fxn--80afpl5a.xn--p1ai%2F%25D0%25B2%25D0%25BE%25D0%25B9%25D1%2582%25D0%25B8%2F; rapidshoplogin=ceb187409f19d79391ec9c7310258a6c; rapidshopemail=egorkin.ivan64%40gmail.com; user_code=grf_21052_5310531',
+    # 'Cookie': 'user_rnd=grf_1705324054_0; _ga=GA1.2.373876291.1705324056; _gid=GA1.2.1440359982.1705324056; _ym_uid=1705324056275299195; _ym_d=1705324056; _ym_isad=1; _ym_visorc=w; rapidshopref=https%3A%2F%2Fxn--80afpl5a.xn--p1ai%2F; rapidshoplogin=8297d8a343692c1dca1202873a1e901c; rapidshopemail=Amaks996%40mail.ru; user_code=grf_23071_1232410; _gat=1; _ga_9TY92ZQNZT=GS1.2.1705324056.1.1.1705324145.0.0.0',
+    'Referer': 'https://xn--80afpl5a.xn--p1ai/%D0%BB%D0%B8%D1%87%D0%BD%D1%8B%D0%B9%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82/',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
     'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
 }
 
 
@@ -38,15 +55,17 @@ session.cookies.update(cookies)
 start = timezone.localtime(timezone.now())
 print(f'Start: {start}')
 
+TABLE_XPATH = '//*[@id="content"]/table/tbody/tr/td/table/tbody/tr[3]/td/table/tbody'
+
 req_amount = 0
 while True:
     if req_amount > 3000:
         exit(0)
         
     try:
-        url = 'http://глиса.рф/личныйкабинет/adlist/0/'
+        url = 'https://xn--80afpl5a.xn--p1ai/%D0%BB%D0%B8%D1%87%D0%BD%D1%8B%D0%B9%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82/adlist/73565/'
         try:
-            page = session.get(url)
+            page = session.get(url, verify=False)
             req_amount += 1
 
             if req_amount % 10000 == 0:
@@ -54,7 +73,8 @@ while True:
         except Exception as e:
             print(f"[REQUEST] {e}")
             continue
-            
+
+        print("Parsing...")
         soup = BeautifulSoup(page.content, 'lxml')
 
         titles = soup.find_all(lambda x: _reg_equal(r'name_\d+', x.get('id')))
